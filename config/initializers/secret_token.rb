c53c93ff9e88d632f88246cd229136125798e05e
tyler-palin <tyler.palin@avantcredit.com>
2013-10-05 11:11:39 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'cd6dbe217c12a99001de221cdd896f9713032df76acdd54102f9114ad04cf95e406ebc54dccf25dfcc83179971720fa5df95da2984536fe302a58fd9d10d1e07'
